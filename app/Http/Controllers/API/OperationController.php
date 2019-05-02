3dcb031e80988ab7a46baa64601bb676f0f3bec1
timolor <t.oloruntoba@outlook.com>
2019-05-02 18:27:13 +0100

    started performanceAggregation method

--- app/Http/Controllers/API/OperationController.php
+++ app/Http/Controllers/API/OperationController.php
@@ -38,4 +38,34 @@ class OperationController extends Controller
         }
         return response()->json(['message' => 'Invalid schedule id','status' => false ], 200);
     }
+
+    public function performanceAggregation(){
+        $validator = \Validator::make($request->all(), [
+            'route_id' => 'required',
+            'airlineCode' => 'required',
+        ]);
+        $error = $validator->errors()->first();
+        if ($validator->fails()) {
+            return response()->json(['message' => $error, 'status' => false ], 200);
+        }
+        
+        $schedule = Schedule::where('route_id',$request->route_id)->where('airlineCode',$request->airline)->get();
+        $percentageArrivals = self::percentageArrivals( $schedule );
+
+
+    }
+
+    public function percentageArrivals($schedule){
+        
+        $totalArrivedSchedule = $schedule->count();
+        $noOfArrivalsOnTime = 0;
+        foreach ($schedule as $key => $value) {
+            $diff_in_minutes = $value->scheduled_arrival_time->diffInMinutes($value->actual_arrival_time);
+            if(($value->scheduled_arrival_time->diffInMinutes($value->actual_arrival_time)<5)){
+                $noOfArrivalsOnTime++;
+            }
+        }
+        $percentageArrivals = ($noOfArrivalsOnTime / $totalArrivedSchedule) * 100;
+    }
+
 }
