b34bb5a65a5729e41b6fcce235a8e5015657a14a
OLORUNTOBA Timilehin <timilehin.oloruntoba@interswitchgroup.com>
2019-05-27 17:56:05 +0100

    migration scaffolding and controller for expense

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('unique_id')->nullable();
             $table->string('terminal')->nullable();
             $table->string('subscription')->nullable();
+            $table->string('location')->nullable();
             $table->decimal('balance',8,2)->default('0');
             $table->tinyInteger('status')->default('0');
             $table->string('email')->unique();
---
+++ database/migrations/2019_05_27_164050_create_expense_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExpenseDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expense_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('expense_id');
+            $table->string('category');           
+            $table->decimal('amount',10,2);
+            $table->string('description');           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expense_details');
+    }
+}
